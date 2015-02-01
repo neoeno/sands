a56e31147b8fabf8cd478b41be9f8ffe32dfd38c
Caden Lovelace <caden@herostrat.us>
2015-02-01 12:12:01 +0000

    Initial Commit

---
+++ sands/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sands
+//
+//  Created by Caden Lovelace on 01/02/2015.
+//  Copyright (c) 2015 Caden Lovelace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
